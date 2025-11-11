026d4993e962ad37cb96d73d31f0a3474cf5100e
Arpad Bajzath <arpad.bajzath@lab49.com>
2025-11-11 11:07:59 +0100

    round 1

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,6 @@ import { createHtmlPlugin } from 'vite-plugin-html'
 import autoprefixer from 'autoprefixer'
 import path from 'path'
 
-export default defineConfig({})
+export default defineConfig({
+  base: '/lab-in-the-dark/',
+})
